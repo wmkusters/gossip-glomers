24fcb6d526b90b9af843df4a26759213944532fa
Bill Kusters <wmkusters@gmail.com>
2026-05-27 16:26:36 -0400

    add flake

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 ## Gossip Glomers
 My solutions to [fly.io's distributed systems exercise problems, Gossip Glomers](https://fly.io/dist-sys/).
+
+To run a given challenge, such as the first challenge, "echo", you can use the Nix flake like so:
+
+``` shell
+nix run .#echo
+```
+
--- flake.nix
+++ flake.nix
@@ -1,16 +1,17 @@
 {
-  description = "Flake for maelstrom shell";
+  description = "Flake for gossip glomers";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
-    # pin nixpkgs to maelstrom 0.2.3
-    nixpkgs.url = "github:NixOS/nixpkgs/59133ee770406f605d61698bc4f1a89efcf461d5";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # pin to maelstrom 0.2.3
+    maelstromPinPkgs.url = "github:NixOS/nixpkgs/59133ee770406f605d61698bc4f1a89efcf461d5";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      maelstromPinPkgs,
     }:
     let
       systems = [
@@ -22,15 +23,56 @@
         builtins.listToAttrs (
           map (system: {
             name = system;
-            value = f nixpkgs.legacyPackages.${system};
+            value = f {
+              pkgs = nixpkgs.legacyPackages.${system};
+              maelstromPkgs = maelstromPinPkgs.legacyPackages.${system};
+            };
           }) systems
         );
+      services = {
+        echo = {
+          subPackage = "echo";
+          bin = "echo";
+          maelstromArgs = "-w echo --node-count 1 --time-limit 10";
+        };
+        unique-id-generation = {
+          subPackage = "unique-id-generation";
+          bin = "unique-id-generation";
+          maelstromArgs = "-w unique-ids --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition";
+        };
+      };
     in
     {
-      devShells = forEachSystem (pkgs: {
-        default = pkgs.mkShell {
-          packages = [ pkgs.maelstrom-clj ];
-        };
-      });
+      packages = forEachSystem (
+        { pkgs, ...}:
+        builtins.mapAttrs (
+          name: cfg:
+          pkgs.buildGoModule {
+            inherit name;
+            src = ./.;
+            subPackages = [ cfg.subPackage ];
+            vendorHash = "sha256-b0Vt2rIgPRjYorCc8qmKPoQL0L3DOGurfftAr58jQHA=";
+          }
+        ) services
+      );
+      apps = forEachSystem (
+        { pkgs, maelstromPkgs }:
+        let
+          system = pkgs.system;
+          sharedRuntimeDeps = [ maelstromPkgs.maelstrom-clj ];
+        in
+        builtins.mapAttrs (name: cfg: {
+          type = "app";
+          program = toString (
+            pkgs.writeShellScript name ''
+              export PATH="${pkgs.lib.makeBinPath sharedRuntimeDeps}:$PATH"
+              exec maelstrom test \
+                --bin ${self.packages.${system}.${name}}/bin/${cfg.bin} \
+                ${cfg.maelstromArgs} \
+                "$@"
+            ''
+          );
+        }) services
+      );
     };
 }
