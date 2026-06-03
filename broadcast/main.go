3f54732dc95dedcb251dcabdb78c9516fc283552
Bill Kusters <wmkusters@gmail.com>
2026-06-03 12:58:58 -0400

    working

--- broadcast/main.go
+++ broadcast/main.go
@@ -46,11 +46,15 @@ type Neighbor struct {
 }
 
 func (ob OutboundBroadcast) ShouldTry() bool {
+	if ob.Msg.Message == nil && (ob.Msg.Messages == nil || len(*ob.Msg.Messages) == 0) {
+		log.Println("found empty message")
+		return false
+	}
 	if ob.LastAttempted == nil {
 		return true
 	}
 	// exponentially backoff on retries but start with a high initial wait period to account for injected latency
-	ms := int(math.Exp2(float64(ob.Attempts + 7)))
+	ms := int(math.Exp2(float64(ob.Attempts + 9)))
 	nextTry := (*ob.LastAttempted).Add(time.Duration(ms) * time.Millisecond)
 	return time.Now().After(nextTry)
 }
@@ -62,39 +66,6 @@ func main() {
 	var nm NeighborMap
 
 	n := maelstrom.NewNode()
-
-	// addToOutbound will add a new message to a neighbor's outbound messages
-	// IT EXPECTS TO BE CALLED WHILE HOLDING THE LOCK.
-	addToOutbound := func(nid string, msg int) {
-		nm.Lock.Lock()
-		defer nm.Lock.Unlock()
-		outbound := nm.Neighbors[nid].Needed
-		ob := OutboundBroadcast{
-			LastAttempted: nil,
-			Attempts:      0,
-			Msg: BroadcastMsg{
-				Type:     "broadcast",
-				Messages: &[]int{},
-			},
-		}
-		if len(outbound) == 0 ||
-			(outbound[len(outbound)-1].Msg.Messages != nil && len(*outbound[len(outbound)-1].Msg.Messages) >= BundleSize) {
-			outbound = append(outbound, ob)
-		} else {
-			ob = outbound[len(outbound)-1]
-		}
-		new := []int{}
-		if ob.Msg.Messages != nil {
-			new = *ob.Msg.Messages
-		}
-		new = append(new, msg)
-		ob.Msg.Messages = &new
-		outbound[len(outbound)-1] = ob
-		nm.Neighbors[nid] = Neighbor{
-			ID:     nid,
-			Needed: outbound,
-		}
-	}
 
 	n.Handle("broadcast_ok", func(msg maelstrom.Message) error {
 		return nil
@@ -135,14 +106,18 @@ func main() {
 			receivedMsgs[nm] = true
 		}
 
+		nm.Lock.Lock()
 		for _, neighbor := range nm.Neighbors {
 			if neighbor.ID == msg.Src {
 				continue
 			}
-			for _, nm := range newMsgs {
-				addToOutbound(neighbor.ID, nm)
+			nm.Neighbors[neighbor.ID] = Neighbor{
+				ID:       neighbor.ID,
+				Outbound: append(neighbor.Outbound, newMsgs...),
+				Pending:  neighbor.Pending,
 			}
 		}
+		nm.Lock.Unlock()
 
 		// Update the message type to return back.
 		rx.Type = "broadcast_ok"
@@ -180,8 +155,9 @@ func main() {
 		}
 		for _, neighbor := range m.Topology[n.ID()] {
 			nm.Neighbors[neighbor] = Neighbor{
-				ID:     neighbor,
-				Needed: []OutboundBroadcast{},
+				ID:       neighbor,
+				Outbound: []int{},
+				Pending:  []OutboundBroadcast{},
 			}
 		}
 		reply := TopologyMsg{
@@ -200,36 +176,46 @@ func main() {
 		for {
 			nm.Lock.Lock()
 			for _, neighbor := range nm.Neighbors {
-				ob := OutboundBroadcast{
-					LastAttempted: nil,
-					Attempts:      0,
-					Msg: BroadcastMsg{
-						Type:     "broadcast",
-						Messages: &neighbor.Outbound,
-					},
+				if len(neighbor.Outbound) > 0 {
+					ob := OutboundBroadcast{
+						LastAttempted: nil,
+						Attempts:      0,
+						Msg: BroadcastMsg{
+							Type:     "broadcast",
+							Messages: &neighbor.Outbound,
+						},
+					}
+					pending := neighbor.Pending
+					nm.Neighbors[neighbor.ID] = Neighbor{
+						ID:       neighbor.ID,
+						Outbound: []int{},
+						Pending:  append(pending, ob),
+					}
 				}
-				pending := nm.Neighbors[neighbor.ID].Pending
-				nm.Neighbors[neighbor.ID] = Neighbor{
-					ID:       neighbor.ID,
-					Outbound: []int{},
-					Pending:  append(pending, ob),
-				}
+				pending := neighbor.Pending
 				for i, p := range pending {
 					if p.ShouldTry() {
+						if p.Attempts >= 1 {
+							log.Printf("trying to broadcast %v for the %d time\n", *p.Msg.Messages, p.Attempts)
+						}
 						t := time.Now()
 						p.LastAttempted = &t
 						p.Attempts++
-						err := n.RPC(neighbor.ID, ob.Msg, func(mmsg maelstrom.Message) error {
+						err := n.RPC(neighbor.ID, p.Msg, func(mmsg maelstrom.Message) error {
 							if mmsg.RPCError() == nil {
 								nm.Lock.Lock()
 								defer nm.Lock.Unlock()
 								for j, sent := range nm.Neighbors[mmsg.Src].Pending {
-									if slices.Equal(*sent.Msg.Messages, *ob.Msg.Messages) {
+									if sent.Msg.Messages == nil || p.Msg.Messages == nil {
+										log.Println("got a bad message")
+										continue
+									}
+									if slices.Equal(*sent.Msg.Messages, *p.Msg.Messages) {
 										old := nm.Neighbors[mmsg.Src].Pending
 										nm.Neighbors[mmsg.Src] = Neighbor{
 											ID:       mmsg.Src,
 											Outbound: nm.Neighbors[mmsg.Src].Outbound,
-											Pending:  append(old[:j], old[j+1:]...),
+											Pending:  slices.Delete(old, j, j+1),
 										}
 									}
 								}
@@ -245,7 +231,7 @@ func main() {
 				}
 			}
 			nm.Lock.Unlock()
-			time.Sleep(10 * time.Millisecond)
+			time.Sleep(50 * time.Millisecond)
 		}
 	}()
 
